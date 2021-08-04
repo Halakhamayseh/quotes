3d9205a5f38a6c7e6dfaf8aa1caeed2a9e11ae99
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-04 18:10:19 +0300

    to push gradle

---
+++ app/src/main/java/quotes/Parentquotes.java
@@ -0,0 +1,11 @@
+package quotes;
+
+import java.util.List;
+
+public class Parentquotes {
+List<Quotes>quotes;
+
+    public Parentquotes(List<Quotes> quotes) {
+        this.quotes = quotes;
+    }
+}
---
+++ app/src/main/java/quotes/Quotes.java
@@ -0,0 +1,16 @@
+package quotes;
+
+public class Quotes {
+String []tags;
+String author;
+String likes;
+String text;
+
+    public Quotes(String[] tags, String author, String likes, String text) {
+        this.tags = tags;
+        this.author = author;
+        this.likes = likes;
+        this.text = text;
+    }
+}
+
